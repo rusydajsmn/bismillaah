a7407b89d001a99d5f9a3520c9543c121fcc56b6
rusydajsmn <rusyda.jasmine.tif420@polban.ac.id>
2022-10-27 10:21:39 +0700

    commit

--- server/app.js
+++ server/app.js
@@ -21,5 +21,8 @@ mongoose.connect(process.env.DB_URI, {
     useCreateIndex: true
 }).then(() => console.log("Connected to the database!")).catch((err) => console.log(err));
 
+//routes prefix
+app.use("/api/post", require("./routes/routes"));
+
 //start server
 app.listen(port, () => console.log(`server running at http://127.0.0.1:${port}`));
